7934b09892a52155d8bc4fc30f7c022f49acb89f
Pooja R <poojarhyr@gmail.com>
2020-02-06 12:49:31 +0530

    Initial Commit

---
+++ TaskRealm/Data Model/Data.swift
@@ -0,0 +1,19 @@
+//
+//  Data.swift
+//  TaskRealm
+//
+//  Created by Pooja R on 06/02/20.
+//  Copyright © 2020 Pooja R. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+class Data: object {
+    
+   @objc dynamic var firstName: String = " "
+   @objc dynamic var lastName: String = " "
+   @objc dynamic var emailID: String = " "
+    
+}
